bd802f39eec6fc7e69008d57095911cafa1bd304
mtsgi <oq@live.jp>
2019-07-11 11:42:20 +0900

    Initial Release

---
+++ defgen.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let DEFINE = {
+        "id": "",
+        "name": "",
+        "icon": "",
+        "version": "",
+        "author": null,
+    
+        "support": {
+            "darkmode": false,
+            "resize": false,
+            "fullscreen": false
+        },
+    
+        "size": {
+            "width": null,
+            "height": null
+        },
+    
+        "view": "",
+        "script": "",
+        "css": ""
+    }
+    document.getElementById("export").addEventListener("click", () => {
+        DEFINE.id = document.getElementById("id").value;
+        DEFINE.name = document.getElementById("name").value;
+        DEFINE.icon = document.getElementById("icon").value;
+        DEFINE.version = document.getElementById("version").value;
+        DEFINE.author = document.getElementById("author").value;
+        DEFINE.view = document.getElementById("view").value;
+        DEFINE.script = document.getElementById("script").value;
+        DEFINE.css = document.getElementById("css").value;
+
+        DEFINE.support.darkmode = document.getElementById("darkmode").checked;
+        DEFINE.support.resize = document.getElementById("resize").checked;
+        DEFINE.support.fullscreen = document.getElementById("fullscreen").checked;
+
+        DEFINE.size.width = document.getElementById("width").value;
+        DEFINE.size.height = document.getElementById("height").value;
+
+        var blob = new Blob([ JSON.stringify(DEFINE, null, 4) ], { type: "application/json" });
+        document.getElementById("export").href = URL.createObjectURL(blob);
+        document.getElementById("export").download = "define.json";
+    })
+});
+  
